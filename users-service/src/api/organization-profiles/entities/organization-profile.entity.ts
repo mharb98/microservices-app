f637d58b41a2726ae6e996af794610e3dff1e30a
mharb98 <marwanharb65@outlook.com>
2023-03-11 20:10:06 +0200

    Added scaffol for social profile

--- users-service/src/api/organization-profiles/entities/organization-profile.entity.ts
+++ users-service/src/api/organization-profiles/entities/organization-profile.entity.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { OrganizationProfile } from '@prisma/client';
+import { Organization, OrganizationProfile } from '@prisma/client';
+import { UserDepartmentEntity } from '../../user-departments/entites/user-department.entity';
+import { UserEntity } from '../../users/entities/user.entity';
 
 export class OrganizationProfileEntity implements OrganizationProfile {
   @ApiProperty({
@@ -43,4 +45,23 @@ export class OrganizationProfileEntity implements OrganizationProfile {
     example: 1,
   })
   organizationId: number;
+
+  @ApiProperty({
+    description: 'Organization entity to which the user belongs',
+    // type:
+  })
+  organization?: Organization;
+
+  @ApiProperty({
+    description: 'User entity associated with organization profile',
+    type: UserEntity,
+  })
+  user?: UserEntity;
+
+  @ApiProperty({
+    description: 'List of departments of user inside the organization',
+    type: UserDepartmentEntity,
+    isArray: true,
+  })
+  departments?: UserDepartmentEntity[];
 }
--- users-service/src/api/organization-profiles/organization-profiles.service.ts
+++ users-service/src/api/organization-profiles/organization-profiles.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { OrganizationProfileEntity } from './entities/organization-profile.entity';
+import { OrganizationProfileRepository } from './repositories/organization-profile-repository.interface';
 
 @Injectable()
-export class OrganizationProfilesService {}
+export class OrganizationProfilesService {
+  constructor(
+    @Inject('OrganizationProfileRepository')
+    private repository: OrganizationProfileRepository,
+  ) {}
+
+  async getOrganizationProfile(
+    organizationProfileId: number,
+  ): Promise<OrganizationProfileEntity> {
+    return await this.repository.findUnique(organizationProfileId);
+  }
+
+  async enableUser(organizationProfileId: number): Promise<void> {
+    await this.repository.findOneAndUpdate(organizationProfileId, {
+      banned: false,
+    });
+  }
+
+  async disableUser(organizationProfileId: number): Promise<void> {
+    await this.repository.findOneAndUpdate(organizationProfileId, {
+      banned: true,
+    });
+  }
+}
