796c89b26699573efebdac3254586ddd0d9da2ea
mharb98 <marwanharb65@outlook.com>
2023-03-17 17:52:57 +0200

    feat: added organization consumption consumers

--- users-service/src/app.controller.ts
+++ users-service/src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ProducerService } from './Brokers/producer.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly producerService: ProducerService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    await this.producerService.produce('organizations', {
+      key: 'create-organization',
+      value: JSON.stringify({ id: 2, name: 'Organization' }),
+    });
+    return 'Hello world';
   }
 }
